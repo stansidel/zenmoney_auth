e43e478c2ff0b58e3f6fef5043665791c35d23a7
stan <sidelnikov.stanislav@gmail.com>
2013-05-15 01:40:11 +0600

    Table view for choosing accounts etc

---
+++ OAuthTest2/GlossAccountsView.h
@@ -0,0 +1,13 @@
+//
+//  GlossAccountsView.h
+//  OAuthTest2
+//
+//  Created by Stan on 13.05.13.
+//  Copyright (c) 2013 GLoSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GlossAccountsView : UITableViewController
+
+@end
